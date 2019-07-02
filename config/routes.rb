0422b2cdc93a3ae1e4401464dc6bc8cd21780eb4
fumikaobayashi <fumika.obayashi@gmail.com>
2019-07-02 15:31:11 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'user/index'
-  get 'dream/index'
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'dreams#index'
+  resources :users, only: [:index, :show]
+  resources :dreams
 end
