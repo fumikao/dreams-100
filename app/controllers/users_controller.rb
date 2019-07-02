a9521ee8ddd201f3c9cc46a6c0cde95e3e97b4d7
fumikaobayashi <fumika.obayashi@gmail.com>
2019-07-02 17:16:53 +0900

    Update create dream

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,17 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @users = User.all.page(params[:page]).per(10)
   end
 
   def show
     @user = User.find(params[:id])
-    @dream = Dream.new
-    @dreams = @user.dreams
+    @dream = @user.dreams.new
+    if @user == current_user
+      @dreams = @user.dreams
+    else
+      @dreams = @user.dreams.where(opened: true)
+    end
   end
 end
