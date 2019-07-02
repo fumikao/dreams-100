58361fbc14f1c0da116fbdaa57578b410294b2e1
fumikaobayashi <fumika.obayashi@gmail.com>
2019-07-02 16:33:32 +0900

    Update user page

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -1,4 +1,18 @@
 class DreamsController < ApplicationController
   def index
   end
+
+  def create
+    dream = current_user.dreams.new(dream_params)
+    if dream.save
+      redirect_to user_path(current_user)
+    else
+      render user_path(current_user)
+    end
+  end
+
+  private
+  def dream_params
+    params.require(:dream).permit(:content, :opened)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all.page(params[:page]).per(10)
   end
 
   def show
     @user = User.find(params[:id])
+    @dream = Dream.new
+    @dreams = @user.dreams
   end
 end
