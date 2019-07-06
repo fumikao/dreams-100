6e724d31311302657828bf8b2c6302ce08f2c301
fumikaobayashi <fumika.obayashi@gmail.com>
2019-07-06 15:49:37 +0900

    Change column

---
+++ db/migrate/20190706063509_change_column_integer.rb
@@ -0,0 +1,15 @@
+class ChangeColumnInteger < ActiveRecord::Migration[5.2]
+  def change
+    def up
+      if Rails.env.development? || Rails.env.test?
+        change_column :dreams, :opened, :integer
+      else Rails.env.production?
+        change_column :dreams, :opened, 'integer USING CAST(opened AS integer)'
+      end
+    end
+  
+    def down
+      change_column :dreams, :opened, :boolean
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_06_062456) do
+ActiveRecord::Schema.define(version: 2019_07_06_063509) do
 
   create_table "dreams", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content", null: false
