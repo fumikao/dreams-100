6bdc67bf811304e045c5c70b8ba75ccefbcbfae2
fumikaobayashi <fumika.obayashi@gmail.com>
2019-07-06 16:21:24 +0900

    Change column

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_06_065024) do
+ActiveRecord::Schema.define(version: 2019_07_06_070049) do
 
   create_table "dreams", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content", null: false
     t.string "status", default: "0", null: false
-    t.integer "opened", default: 1, null: false
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "row_order"
+    t.integer "opened"
     t.index ["user_id"], name: "index_dreams_on_user_id"
   end
 
