ae1f77ece24ea3910b969bb24a63f7118385188d
fumikaobayashi <fumika.obayashi@gmail.com>
2019-07-07 12:50:40 +0900

    Remove migration

--- db/migrate/20190702083223_change_data_opened_of_dreams.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDataOpenedOfDreams < ActiveRecord::Migration[5.2]
-  def change
-    change_column :dreams, :opened, :boolean
-  end
-end
--- db/migrate/20190706095335_change_opened.rb
+++
@@ -1,5 +0,0 @@
-class ChangeOpened < ActiveRecord::Migration[5.2]
-  def change
-    change_column :dreams, :opened, :string
-  end
-end
