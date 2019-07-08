8d07b7990ee8ee6a63bcb652744c885161d975b0
fumikaobayashi <fumika.obayashi@gmail.com>
2019-07-08 10:04:03 +0900

    Recreate dreams and likes

---
+++ db/migrate/20190708010132_deop_dreams.rb
@@ -0,0 +1,5 @@
+class DeopDreams < ActiveRecord::Migration[5.2]
+  def change
+    drop_table :dreams
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_07_035438) do
+ActiveRecord::Schema.define(version: 2019_07_08_010300) do
 
   create_table "dreams", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content", null: false
     t.string "status", default: "0", null: false
     t.string "opened", default: "1", null: false
+    t.integer "row_order", null: false
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "row_order"
     t.index ["user_id"], name: "index_dreams_on_user_id"
+  end
+
+  create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "dream_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["dream_id"], name: "index_likes_on_dream_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -37,4 +46,6 @@ ActiveRecord::Schema.define(version: 2019_07_07_035438) do
   end
 
   add_foreign_key "dreams", "users"
+  add_foreign_key "likes", "dreams"
+  add_foreign_key "likes", "users"
 end
