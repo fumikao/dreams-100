d9352bea1d47cc793bcb598b42dc17808ab6de2f
fumikaobayashi <fumika.obayashi@gmail.com>
2019-07-08 09:42:29 +0900

    Update row_order

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_07_035532) do
+ActiveRecord::Schema.define(version: 2019_07_08_003740) do
 
   create_table "dreams", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content", null: false
     t.string "status", default: "0", null: false
     t.string "opened", default: "1", null: false
-    t.integer "row_order"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "row_order"
     t.index ["user_id"], name: "index_dreams_on_user_id"
   end
 
