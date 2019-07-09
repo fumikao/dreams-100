5b3418e59fc64a1c4f2a999f6bec4f7c4788285f
fumikaobayashi <fumika.obayashi@gmail.com>
2019-07-09 13:11:03 +0900

    Ajax likes function

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,12 @@
 class LikesController < ApplicationController
   def create
-    dream = Dream.find(params[:dream_id])
-    like = Like.create(user_id: current_user.id, dream_id: dream.id)
-    redirect_to user_path(dream.user)
+    @dream = Dream.find(params[:dream_id])
+    @like = Like.create(user_id: current_user.id, dream_id: @dream.id)
   end
   
   def destroy
-    dream = Dream.find(params[:dream_id])
-    like = Like.find_by(user_id: current_user.id, dream_id: dream.id)
-    like.destroy
-    redirect_to user_path(dream.user)
+    @dream = Dream.find(params[:dream_id])
+    @like = Like.find_by(user_id: current_user.id, dream_id: @dream.id)
+    @like.destroy
   end
 end
