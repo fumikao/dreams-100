4a3eeba28b031c6b749fc4a9a8f03cf002729a62
fumikaobayashi <fumika.obayashi@gmail.com>
2019-07-17 10:27:12 +0900

    Little change

--- app/assets/javascripts/dreams_sort.js
+++ app/assets/javascripts/dreams_sort.js
@@ -6,7 +6,6 @@ $(function(){
       let item_data = item.data();
       let params = {_method: 'put'};
       params[item_data.modelName] = { row_order_position: item.index() }
-
       $.ajax({
         type: 'PUT',
         url: item_data.updateUrl,
